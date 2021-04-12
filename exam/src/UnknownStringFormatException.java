d04b84c8793472b72e4ba2b4aef8c43e51db83cc
Egor Erov <74404082+iamerove@users.noreply.github.com>
2021-04-13 01:20:24 +0300

    Delete exam directory

--- exam/src/UnknownStringFormatException.java
+++
@@ -1,5 +0,0 @@
-public class UnknownStringFormatException extends Exception {
-    public UnknownStringFormatException(String message, int position) {
-        super(String.format("error in substring %d: %s", position, message));
-    }
-}
